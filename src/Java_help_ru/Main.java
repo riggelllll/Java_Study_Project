678895c66f56d5f424c1c8070a581dd47df1abf6
Vladyslav Koniukhov <riggelllll@gmail.com>
2018-12-12 09:45:43 +0200

    Решен Task1.

---
+++ src/Java_help_ru/Main.java
@@ -0,0 +1,12 @@
+package Java_help_ru;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args)throws IOException {
+
+        Task1 task1 = new Task1();
+        task1.enterText();
+    }
+}
--- src/Java_help_ru/Task1.java
+++ src/Java_help_ru/Task1.java
@@ -2,6 +2,30 @@
 
 package Java_help_ru;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task1 {
+    InputStreamReader reader;
+    StringBuffer buffer;
+
+    Task1(){
+     reader = new InputStreamReader(System.in);
+     buffer = new StringBuffer();
+    }
+
+    public void enterText() throws IOException {
+       char t;
+       int count = 0;
+       do {
+           t = (char)reader.read();
+           buffer.append(t);
+       }while (t != '.');
+
+       for (int i = 0; i < buffer.length(); i++){
+           if (buffer.charAt(i) == ' ') count++;
+       }
+       System.out.println(count);
+
+    }
 }
